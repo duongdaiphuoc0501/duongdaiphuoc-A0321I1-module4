fcb0ff4b12b4cc83211eb498240f08071b4248d6
duongdaiphuoc0501 <phuocrider25@gmail.com>
2021-11-21 10:46:46 +0700

    update bt data binding

--- Data_Binding_And_Form/bai_tap/mail_dien_tu/src/main/java/com/example/mail_dien_tu/controller/InfController.java
+++ Data_Binding_And_Form/bai_tap/mail_dien_tu/src/main/java/com/example/mail_dien_tu/controller/InfController.java
@@ -6,31 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class InfController {
-    @Autowired
-    private InfRepository infRepository;
-
     @GetMapping("/")
-    public ModelAndView index() {
-        ModelAndView modelAndView = new ModelAndView("home", "inf", new Information());
-        return modelAndView;
-    }
-
-    @GetMapping("/inf/infomation")
-    public String create(Model model) {
-        model.addAttribute("inf", new Information());
-        return "/home";
+    public String showView(){
+        return "home";
     }
 
     @PostMapping("/inf/save")
-    public String save(Information information) {
-        infRepository.save(information);
-        return "redirect:/";
+    public String save(@RequestParam("language") String language,@RequestParam("size") String size, @RequestParam("fillter") String fillter,@RequestParam("signature") String signature,Model model) {
+        model.addAttribute("inf", new Information(language, size, fillter, signature));
+        return "information";
     }
 }
